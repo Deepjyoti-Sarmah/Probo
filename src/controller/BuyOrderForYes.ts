0789cc3105bfca269946fa2100e5abbc1514c9ba
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-18 20:07:22 +0530

    adding buy order route

--- src/controller/BuyOrderForYes.ts
+++ src/controller/BuyOrderForYes.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { symbolExists, userExists } from "../types";
+import { INR_BALANCES, ORDERBOOK } from "..";
 
 export const BuyOrderForYes = async (req: Request, res: Response) => {
 
@@ -37,8 +38,39 @@ export const BuyOrderForYes = async (req: Request, res: Response) => {
 
         const totalCost = quantity * price;
 
+        if (INR_BALANCES[userId].balance < totalCost) {
+            res.status(400).json({
+                success: false,
+                message: "Insufficiant balance"
+            })
+            return
+        }
 
+        INR_BALANCES[userId].balance -= totalCost;
+        INR_BALANCES[userId].lockd += totalCost;
 
+        if (!ORDERBOOK[stockSymbol][stockType][price]) {
+            ORDERBOOK[stockSymbol][stockType][price] = {
+                total: 0,
+                orders: {}
+            }
+        }
+
+        ORDERBOOK[stockSymbol][stockType][price].total += quantity;
+        ORDERBOOK[stockSymbol][stockType][price].orders[userId] = (ORDERBOOK[stockSymbol][stockType][price].orders[userId] || 0) + quantity;
+
+        res.status(200).json({
+            success: true,
+            message: "Buy order placed successfully",
+            order: {
+                userId,
+                stockSymbol,
+                stockType,
+                price,
+                totalCost,
+                quantity
+            }
+        })
     } catch (error) {
         res.status(500).json({
             success: false,
