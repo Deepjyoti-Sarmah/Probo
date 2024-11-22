d03fa6f89ce4156e967ed22d9b72bea46157e320
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-22 20:06:57 +0530

    fixing comments

--- src/index.ts
+++ src/index.ts
@@ -73,22 +73,6 @@ export const STOCK_BALANCE: any = {}
 //   }
 // }
 
-// interface StockEntry {
-//   quantity: number;
-//   locked: number;
-// }
-
-// interface SymbolData {
-//   yes?: StockEntry;
-//   no?: StockEntry;
-// }
-
-// interface stockBalance {
-//   [userId: string]: {
-//     [symbol: string]: SymbolData;
-//   };
-// }
-
 // const STOCK_BALANCE: stockBalance = {
 //   "user1": {
 //     "BTC_USDT_10_Oct_2024_9_30": {
@@ -108,73 +92,6 @@ export const STOCK_BALANCE: any = {}
 //   }
 // }
 
-// app.post("/user/create/:userId", (req: any, res: any) => {
-//   const { userId } = req.params;
-
-//   if (INR_BALANCES[userId]) {
-//     return res.status(400).json({
-//       error: "User already exists"
-//     });
-//   }
-
-//   INR_BALANCES[userId] = { balance: 0, locked: 0 };
-
-//   res.status(201).json({
-//     message: `User ${userId} created successfully`,
-//     userId: INR_BALANCES[userId]
-//   });
-// });
-
-// app.post("/symbol/create/:stockSymbol", (req: any, res: any) => {
-//   const { stockSymbol } = req.params;
-
-//   for (const userId in STOCK_BALANCE) {
-//     if (STOCK_BALANCE[userId][stockSymbol]) {
-//       return res.status(400).json({
-//         error: `Symbol ${stockSymbol} already exists for user ${userId}`
-//       });
-//     }
-
-//     STOCK_BALANCE[userId][stockSymbol] = {
-//       yes: { quantity: 0, locked: 0 },
-//       no: { quantity: 0, locked: 0 }
-//     };
-//   }
-
-//   res.status(201).json({
-//     message: `Symbol ${stockSymbol} created successfully for all user`,
-//     symbol: stockSymbol,
-//     stockBalance: STOCK_BALANCE
-//   });
-// });
-
-// app.get("/orderbook", (req, res) => {
-//   res.status(200).json({
-//     orderBook: ORDERBOOK
-//   });
-// });
-
-// app.get("/balance/inr", (req, res) => {
-//   res.status(200).json({
-//     inrBalance: INR_BALANCES
-//   });
-// });
-
-// app.get("/balance/stock", (req, res) => {
-//   res.status(200).json({
-//     STOCK_BALANCE: STOCK_BALANCE
-//   });
-// });
-
-// app.post("/reset", (req, res) => {
-//   Object.keys(INR_BALANCES).forEach(key => delete INR_BALANCES[key]);
-//   Object.keys(STOCK_BALANCE).forEach(key => delete STOCK_BALANCE[key]);
-//   Object.keys(ORDERBOOK).forEach(key => delete ORDERBOOK[key]);
-
-//   res.status(200).json({
-//     message: "All in-memory data structures have been reset."
-//   });
-// });
 
 app.use("/api/v1", router);
 
