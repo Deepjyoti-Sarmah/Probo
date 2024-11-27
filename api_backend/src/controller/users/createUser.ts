aebcb8ed37c9dc0dbb340009dc4249f940a12a26
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-27 16:31:34 +0530

    code refactoring

--- api_backend/src/controller/users/createUser.ts
+++ api_backend/src/controller/users/createUser.ts
@@ -6,58 +6,24 @@ import { client } from "../../redis";
 
 export const createUserEntry = async (req: Request, res: Response) => {
 
-    const { userId } = req.params;
+  const { userId } = req.params;
 
-    // if (!userId) {
-    //     res.status(400).json({
-    //         message: "userId not defined"
-    //     })
-    //     return
-    // }
+  const userObj = {
+    type: "createUser",
+    requestType: "user",
+    userId
+  }
 
-    // try {
-    //     if (userExists(userId)) {
-    //         res.status(400).json({
-    //             success: false,
-    //             message: "User already exists"
-    //         });
-    //         return;
-    //     }
+  try {
+    const pubSub = handlePubSubWithTimeout("user", 5000);
+    await client.lPush("taskQueue", getJsonStringifyData(userObj));
+    const response = await pubSub;
+    sendResponse(res, response);
 
-    //     const user = INR_BALANCES[userId] = {
-    //         balance: 0,
-    //         locked: 0
-    //     };
+  } catch (error: any) {
+    res.status(500).json({
+      error: error.message || "Error in pubsub connumnication"
+    })
 
-    //     res.status(200).json({
-    //         success: true,
-    //         message: "User created successfully",
-    //         data: user
-    //     });
-
-    // } catch (error) {
-    //     res.status(500).json({
-    //         message: "Internal server error"
-    //     });
-    //     return
-    // }
-
-    const userObj = {
-        type: "createUser",
-        requestType: "user",
-        userId
-    }
-
-    try {
-        const pubSub = handlePubSubWithTimeout("user", 5000);
-        await client.lPush("taskQueue", getJsonStringifyData(userObj));
-        const response = await pubSub;
-        sendResponse(res, response);
-
-    } catch (error: any) {
-        res.status(500).json({
-            error: error.message || "Error in pubsub connumnication"
-        })
-
-    }
+  }
 }
