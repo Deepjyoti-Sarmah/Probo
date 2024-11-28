84d05c246680cfb32f5f0c9ea4d77045381197c7
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-28 17:00:56 +0530

    adding middlewares

--- hono_backend/api_backend/src/env.ts
+++ hono_backend/api_backend/src/env.ts
@@ -1,7 +1,8 @@
 import { z } from "zod"
 
 const EnvSchema = z.object({
-  PORT: z.coerce.number().default(3000)
+  PORT: z.coerce.number().default(3000),
+  JWT_SECRET: z.string().min(6)
 })
 
 export type env = z.infer<typeof EnvSchema>;
