9a3f58c7301115e890d30991e5f8d35f1672a924
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-12-02 20:40:42 +0530

    adding onrampInr for user route

--- V2/hono_backend/api_backend/src/controllers/user/onRampInr.controllers.ts
+++ V2/hono_backend/api_backend/src/controllers/user/onRampInr.controllers.ts
@@ -1,4 +1,7 @@
 import type { Context } from "hono"
+import { OnRampInrSchema } from "../../schemas/index.schemas.js";
+import { getJsonStringifyData, handlePubSubWithTimeout, sendResponse, TaskQueue, TimeOutMs } from "../../utils/index.utils.js";
+import { client } from "../../redis.js";
 
 export const onRampInr = async (c: Context) => {
   const parseData = OnRampInrSchema.safeParse(c.req.json())
@@ -9,6 +12,16 @@ export const onRampInr = async (c: Context) => {
   try {
     const { amount } = parseData.data;
 
+    const onRampInrObject = {
+      type: "onRampInr",
+      payload: {
+        amount
+      }
+    }
+
+    const response = await handlePubSubWithTimeout("onRampInr", TimeOutMs)
+    await client.lPush(TaskQueue, getJsonStringifyData(onRampInrObject))
+    sendResponse(c, response)
   } catch (error) {
     return c.json({ message: "error while onramping Inr" }, { status: 500 })
   }
