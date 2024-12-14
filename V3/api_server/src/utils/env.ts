edc52838f3e398185525be4283afc547875d8d3e
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-12-14 21:47:47 +0530

    adding redis env

--- V3/api_server/src/utils/env.ts
+++ V3/api_server/src/utils/env.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  JWT_SECRET: z.string().min(6).default("asd123jkl789")
+  JWT_SECRET: z.string().min(6).default("asd123jkl789"),
+  REDIS_URL: z.string().default("redis://localhost:6379")
 })
 
 export type env = z.infer<typeof EnvSchema>
