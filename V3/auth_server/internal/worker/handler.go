5355ecdeb16dd2fa6bd8297a2082702d7ad9a8ae
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-12-18 20:50:16 +0530

    adding user registration

--- V3/auth_server/internal/worker/handler.go
+++ V3/auth_server/internal/worker/handler.go
@@ -19,6 +19,11 @@ func (s *Service) handleUserRegistration(ctx context.Context, payload map[string
 		return fmt.Errorf("failed to hash passord: %w", err)
 	}
 
+	_, err = s.server.DB().ExecContext(ctx, "INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)", username, email, hashedPassword, role)
+	if err != nil {
+		return fmt.Errorf("failed to save user: %w", err)
+	}
+
 	token, err := auth.GenerateToken(username, role)
 	if err != nil {
 		return fmt.Errorf("failed to generate token: %w", err)
@@ -30,8 +35,6 @@ func (s *Service) handleUserRegistration(ctx context.Context, payload map[string
 			"email":    email,
 			"role":     role,
 			"token":    token,
-			//TODO: remove this
-			"password": hashedPassword,
 		},
 	}
 
